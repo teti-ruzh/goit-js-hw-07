1f7b22ec455bb9bfdddc2281441bfd1b13829457
Tetiana Ruzhetska <tetiana.ruzhetska@gmail.com>
2023-02-13 14:40:26 +0200

    Comply Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
+
+function createGalleryMarkup(images) {
+return images.map(({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}).join('')
+};
+
+
+  var lightbox = new SimpleLightbox('.gallery__item', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
+
 console.log(galleryItems);
